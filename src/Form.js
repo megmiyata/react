427bb1b4c950752c4c2a82430b0371e2c97f9e24
megmiyata <megmiyata@hotmail.com>
2020-02-19 16:43:13 -0300

    first commit

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class From extends React.Component {
+    render() {
+        return (
+            <div> 
+                <label>Nome</label>
+                <input>
+
+                <label>GitHub</label>
+                <input>
+
+                
+            </div>
+        )
+    }
+}
---
+++ src/TableProfissionais.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TableProfissionais extends React.Component {
+    render() {
+        return(
+            <h2>Eu sou uma tabela</h2>
+        )
+    }
+}
+
+export default TableProfissionais;
